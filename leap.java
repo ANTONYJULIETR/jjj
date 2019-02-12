ff26f398ac2b08fc9ae7f05f67a76a0683dcfd20
ANTONYJULIETR <43901608+ANTONYJULIETR@users.noreply.github.com>
2019-02-12 13:58:01 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+public class LeapYear 
+{
+
+    public static void main(String[] args) {
+
+        int year = 2013;
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                    if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println(year + " Yes");
+        else
+            System.out.println(year + "No");
+    }
+}
